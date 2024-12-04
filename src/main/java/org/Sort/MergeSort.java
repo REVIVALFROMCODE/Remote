5f4cad1e63e9dfda63b16ad9475a653fa9da807a
Your Name <you@example.com>
2024-12-04 07:42:30 +0000

    1

---
+++ src/main/java/org/Sort/MergeSort.java
@@ -0,0 +1,14 @@
+package org.Sort;
+
+public class MergeSort {
+    private Comparable[] aux;
+    private void merge(Comparable[] a, int lo, int mid, int hi){
+        int i = lo, j=mid+1;
+        for(int k=lo; k<=hi; k++){
+            aux[k]=a[k];
+        }
+        for(int k=lo; k<=hi; k++){
+            if(i>mid)
+        }
+    }
+}
